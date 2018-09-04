27552a700e41213c724349883aca4acc0c594a62
LEO <guyuleo@gmail.com>
2018-09-04 22:33:28 +0800

    upload com update

--- dva-demo/src/components/upLoad.js
+++ dva-demo/src/components/upLoad.js
@@ -1,23 +1,5 @@
-import React from 'react';
-import { Upload, Icon, message } from 'antd';
-
-function getBase64(img, callback) {
-  const reader = new FileReader();
-  reader.addEventListener('load', () => callback(reader.result));
-  reader.readAsDataURL(img);
-}
-
-function beforeUpload(file) {
-  const isJPG = file.type === 'image/jpeg';
-  if (!isJPG) {
-    message.error('You can only upload JPG file!');
-  }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
-  }
-  return isJPG && isLt2M;
-}
+import React from 'react'
+import { Upload, Icon, message } from 'antd'
 
 class UpLoad extends React.Component {
   constructor(props) {
@@ -27,67 +9,67 @@ class UpLoad extends React.Component {
     }
   }
 
-  // UNSAFE_componentWillMount(nextProps) {
-  //   console.log('in UpLoad UNSAFE_componentWillMount-------->', this.props, nextProps);
-  // }
-
   handleChange = info => {
-    console.log('handleChange info990099090---------->', info);
+    console.log('handleChange info990099090---------->', info)
     if (info.file.status === 'uploading') {
-      this.setState({ loading: true });
-      return;
+      this.setState({ loading: true })
+      return
     }
+  }
 
-    // if (info.file.status === 'done') {
-    //   getBase64(info.file.originFileObj, imageUrl => this.setState({
-    //     imageUrl,
-    //     loading: false,
-    //   }));
-    // }
-
+  beforeUpload = file => {
+    const isJPG = file.type === 'image/jpeg'
+    if (!isJPG) {
+      message.error('You can only upload JPG file!')
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!')
+    }
+    return isJPG && isLt2M
   }
 
   customRequest = info => {
-    console.log('customRequest info------------>', info);
+    const { handleUpload } = this.props
     const formData = new FormData()
     formData.append('image', info.file)
-    formData.append('https', `https://f1.otosaas.com/openPlatform/img/${ info.file.name }`)
+    formData.append('https', `https://f1.otosaas.com/openPlatform/img/${ new Date().getTime() }`)
     fetch('http://dpp.boluome.com/https/generate', { body: formData, method: 'post' })
     .then(response => response.json())
     .then(response => {
       const { code, https } = response
       if (code === 0) {
         this.setState({ imageUrl: https, loading: false })
-        console.log('https--------->', https);
+        handleUpload && handleUpload(https)
       }
     })
     .catch(error => console.error('Error:', error))
   }
 
   render() {
-    const uploadButton = (
+    const { CustomizeButton } = this.props
+    const uploadButton = CustomizeButton ? React.cloneElement(CustomizeButton, { state: this.state }) : (
       <div>
-        <Icon type={this.state.loading ? 'loading' : 'plus'} />
+        <Icon type={ this.state.loading ? 'loading' : 'plus'} />
         <div className="ant-upload-text">Upload</div>
       </div>
-    );
-    const imageUrl = this.state.imageUrl;
+    )
+    const imageUrl = this.state.imageUrl
 
     return (
-      <div className='UpLoadCom-container'>
         <Upload
-          name="avatar"
-          listType="picture-card"
-          className="avatar-uploader"
+          name='avatar'
+          listType='picture-card'
+          className='avatar-uploader'
           showUploadList={ false }
-          beforeUpload={ beforeUpload }
+          beforeUpload={ this.beforeUpload }
           onChange={ this.handleChange }
           customRequest={ this.customRequest }
+          style={ this.props.style }
         >
           { imageUrl ? <img src={ imageUrl } alt="avatar" /> : uploadButton }
         </Upload>
-      </div>
-    );
+    )
   }
 }
 
--- dva-demo/src/routes/UpLoad.js
+++ dva-demo/src/routes/UpLoad.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { connect } from 'dva';
 import UpLoadCom from '../components/upLoad'
+import { Icon } from 'antd';
+
+const UploadButton = ({ state }) => {
+  console.log('state----------->', state)
+  return (
+    <div className='UploadButton-container'>
+      <Icon type={ state.loading ? 'loading' : 'plus' } />
+      <div className="ant-upload-text">选择上传图片</div>
+    </div>
+  )
+}
 
 const UpLoad = props => {
   console.log('UpLoad props----------->', props);
+  const handleUpload = imgUrl => {
+    console.log('in handleUpload------------>', imgUrl)
+  }
   return (
     <div className='UpLoadCom-container'>
-      <UpLoadCom props={ props } />
+      <UpLoadCom handleUpload={ handleUpload } CustomizeButton={ <UploadButton /> } />
     </div>
   );
 }
